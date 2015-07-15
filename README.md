00e081e26cd00801cda6eaeee8d00e55cf9f3a61
michawei <freda0330@yahoo.com.tw>
2015-07-15 23:36:21 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # URPO
+	Delta intern 2015.
+## How to use Github
+1. 如何複製下來
+'''
+git clone https://github.com/michawei/URPO
+'''
---
+++ urpoApp_server/models/FilterUse.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+
+var ProjectOwnerSchema = new mongoose.Schema({
+    Name: String,
+    EmpID: String
+});
+
+var FileSchema = new mongoose.Schema({
+    file_name: String,
+    file_path: String
+});
+
+var MilestoneSchema = new mongoose.Schema({
+    name: String,
+    start_time: { type: Date, default: Date.now },
+    end_time: { type: Date, default: Date.now },
+    status: String,
+    description: String,
+    file: [FileSchema]
+});
+
+var PrincipalInvestigatorSchema = new mongoose.Schema({
+    Name: String,
+    Email: String,
+    Website: String
+});
+
+var EditStateSchema = new mongoose.Schema({
+    add_or_edit: String,
+    edit_person: String,
+    edit_column: String
+});
+
+var FilterUseSchema = new mongoose.Schema({
+    //id: String,
+    Category: [String],
+    Dept: [String],
+    //Project_Name: String,
+    //Project_Description: String,
+    //Estimated_Start_Date: { type: Date, default: Date.now },
+    //Estimated_End_Date: { type: Date, default: Date.now },
+    Region: [String],
+    Institution: [String],
+    //Competence: String,
+    Collaboration_Model: [String],
+    Project_Agreement_Status: [String],
+    Currency: [String],
+    //Cash_Funding: String,
+    //Project_Owners: [ProjectOwnerSchema],
+    //Principal_Investigators: [PrincipalInvestigatorSchema],
+    //Milestone: [MilestoneSchema],
+    //edit_state: [EditStateSchema],
+    //memo: String,
+    //file_system: {}
+});
+
+module.exports = mongoose.model('FilterUse', FilterUseSchema);
