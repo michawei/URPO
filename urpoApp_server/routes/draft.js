04db4471b2ebd1dab32aaf3014c71380e316d8f8
Brian Huang <b01902068@ntu.edu.tw>
2015-07-17 11:16:58 +0800

    modify draft.js

--- urpoApp_server/routes/draft.js
+++ urpoApp_server/routes/draft.js
@@ -21,6 +21,46 @@ router.get('/:id', function(req, res, next) {
   	});
 });
 
+/*router.get('/sample', function(req, res) {
+	_data = [
+		{
+			Category: "IoT",
+    		Dept: "Urpo",
+    		Project_Name: "智慧監控",
+    		Project_Description: "無",
+    		//Edit_Date: { type: Date, default: Date.now },
+    		Project_Owners: [{Name: "Brian", EmpID: "51001"}, {Name: "Coke", EmpID: "1991"}],
+    		Region: "Taiwan",
+    		Institution: "NCKU",
+    		Competence: "first",
+    		Principal_Investigators: [{Name: "PupuDr", Email: "a@gmail.com", Website: "agar.io"}, {Name: "Pri", Email: "c@delta.com.tw", Website: "arag.io"}],
+    		Collaboration_Model: "3-4",
+    		Project_Agreement_Status: "none",
+    		memo: "Hello World!",
+    		edit_state: [{add_or_edit: "add", edit_person: "Me", edit_column: "3"}, {add_or_edit: "edit", edit_person: "Hey", edit_column: "2, 5"}],
+    		file_system: {}
+		},
+		{
+			Category: "server",
+    		Dept: "D2D",
+    		Project_Name: "server",
+    		Project_Description: "無",
+    		//Edit_Date: { type: Date, default: Date.now },
+    		Project_Owners: [{Name: "Brian", EmpID: "51001"}, {Name: "Coke", EmpID: "1991"}],
+    		Region: "Taiwan",
+    		Institution: "NCCU",
+    		Competence: "first",
+    		Principal_Investigators: [{Name: "DG", Email: "a@gmail.com", Website: "agar.io"}, {Name: "CR", Email: "c@delta.com.tw", Website: "arag.io"}],
+    		Collaboration_Model: "3-4",
+    		Project_Agreement_Status: "none",
+    		memo: "Hello World!",
+    		edit_state: [{add_or_edit: "add", edit_person: "CS", edit_column: "3"}, {add_or_edit: "edit", edit_person: "Jade", edit_column: "2, 3, 4"}],
+    		file_system: {}
+		}
+	];
+    res.json(_data);  
+});*/
+
 /* Add/Edit頁面 */
 router.post('/', function(req, res, next) {
 	draft.create(req.body, function(err, post) {
