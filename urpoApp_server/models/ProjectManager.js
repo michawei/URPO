9c7c8b36877873c0f0724e3458fe92cd0197857c
michawei <freda0330@yahoo.com.tw>
2015-08-08 00:06:45 +0800

    version2 of URPO server

--- urpoApp_server/models/ProjectManager.js
+++ urpoApp_server/models/ProjectManager.js
@@ -4,12 +4,12 @@ var ProjectManagerSchema = new mongoose.Schema({
     name: String,
     username: { type: String, unique: true },
     password: String,
-    EMPID: String,
-    Project_ID: [String],
-    Draft_ID: [String],
-    Profile: String,    //存路徑
-    memo: String,
-    role: String
+    id: String,
+    projectID: [String],
+    //Draft_ID: [String],
+    profile: String,    //存路徑
+    remark: String,
+    //role: String
 });
 
 module.exports = mongoose.model('ProjectManager', ProjectManagerSchema);
